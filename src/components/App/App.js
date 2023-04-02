6145f489da4e4eccaaf6a3a97c79b364ca6dc276
Maksymov-Serhii <maksymov.serhii.10.01@gmail.com>
2023-04-03 01:22:43 +0300

    changed App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className='title'>CyberBionic systematics</h1>
+      <h3 className='title'>Welcome</h3>
     </div>
   );
 }
