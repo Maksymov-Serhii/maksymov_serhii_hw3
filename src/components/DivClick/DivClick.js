a4fb6077abe872dfd89afb887b649f695c8cee1a
Maksymov-Serhii <maksymov.serhii.10.01@gmail.com>
2023-04-03 02:48:22 +0300

    DivClicked component changed

--- src/components/DivClick/DivClick.js
+++ src/components/DivClick/DivClick.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./divClick.css"
 
 class DivClick extends React.Component {
+  constructor() {
+    super();
+    this.state = { lastClickedElement: '' };
+  }
   render() {
     return (
-      <div className="some-component" onClick={() => console.log("Div clicked")}>
+      <div className="some-component" onClick={() => {
+        console.log("Div clicked");
+        this.setState({ lastClickedElement: "Div clicked" })
+        }}>
+
         <h2>Clickable component</h2>
+        <p>{this.state.lastClickedElement}</p>
+        
         <button className="div-click-button" onClick={(event) => {
           event.stopPropagation();
           console.log("Button clicked");
+          this.setState({ lastClickedElement: "Button clicked" });
         }}>Click me, please.</button>
       </div>
     )
